28234514adbaca1d2e1ce4569ea46c6725e7cdde
alina <alina.daduts@gmail.com>
2022-05-20 11:20:47 +0300

    задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = "Ivanovch";
+        String lastName = "Ivanov";
+        System.out.printf("ФИО сотрудника — %s %s %s%n", lastName,firstName,middleName);
+
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        fullName = "Иванов Семён Семёнович";
+        String[] names = fullName.split(" ");
+        System.out.print("Данные ФИО сотрудника — ");
+        for (int i = 0; i< names.length; i++) {
+            String name = names[i];
+            if(name.contains("ё")){
+                int index = name.indexOf('ё');
+                names[i]=name.substring(0,index)+"е"+ name.substring(index+1);
+            }
+            if (i == name.length() - 1){
+                System.out.print(names[i]);
+            }else {
+                System.out.print(names[i]+" ");
+            }
+        }
+
     }
 }
